ec0369f9acfaa5c7669aa0a4348b046027d4c952
deparr <dparrott@protonmail.com>
2025-10-12 17:54:33 -0600

    rename cli main

--- build.zig
+++ build.zig
@@ -15,7 +15,7 @@ pub fn build(b: *std.Build) void {
         const exe = b.addExecutable(.{
             .name = "qoi",
             .root_module = b.createModule(.{
-                .root_source_file = b.path("src/main.zig"),
+                .root_source_file = b.path("src/tool.zig"),
                 .target = target,
                 .optimize = optimize,
             }),
